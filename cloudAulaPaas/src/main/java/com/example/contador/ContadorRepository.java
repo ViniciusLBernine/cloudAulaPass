08f89a3dcb4e0da83386510be2f7d8f44489260e
ViniciusLBernine <137361209+ViniciusLBernine@users.noreply.github.com>
2025-06-26 22:23:27 -0300

    Delete cloudAulaPaas directory

--- cloudAulaPaas/src/main/java/com/example/contador/ContadorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.contador;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ContadorRepository extends MongoRepository<Contador, String> {
-}
